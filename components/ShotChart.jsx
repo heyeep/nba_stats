7f415c9751e608ff5934a3ef9cd7593eac0246ac
Hiep Nguyen <hiepism@gmail.com>
2021-10-29 10:23:13 -0700

    Add functionality to parse the rowSet

--- components/ShotChart.jsx
+++ components/ShotChart.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import Head from 'next/head'
 import { getShots } from '../actions'
 
+import {shotsToJson} from '../helpers/util'
+
 class ShotChart extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      shots: []
+    }
   }
   async componentDidMount() {
     try {
       const response = await getShots()
       if (response.status === 200) {
-        console.log(response)
+        const rows = response.data.resultSets[0].rowSet
+        this.setState({shots: shotsToJson(rows)})
       }
     } catch (err) {
       console.error(err)
     }
+    console.log(this.state.shots)
   }
 
   render() {
---
+++ helpers/util.jsx
@@ -0,0 +1,34 @@
+// Data from shotchartdetail is an array of arrays.
+// This functions labels each element in that array.
+export const shotsToJson = (set) => {
+  let shots = []
+  for (let row = 0; row < set.length; row++) {
+    let shot = {
+      gridType: set[row][0],
+      gameId: set[row][1],
+      gameEventId: set[row][2],
+      playerId: set[row][3],
+      teamId: set[row][4],
+      teamName: set[row][5],
+      period: set[row][6],
+      minutesRemaining: set[row][7],
+      secondsRemaining: set[row][8],
+      eventType: set[row][9],
+      actionType: set[row][10],
+      shotType: set[row][11],
+      shotZoneBasic: set[row][12],
+      shotZoneArea: set[row][13],
+      shotZoneRange: set[row][14],
+      shotDistance: set[row][16],
+      locX: set[row][17],
+      locY: set[row][18],
+      shotAttemptedFlag: set[row][19],
+      shotMadeFlag: set[row][20],
+      gameDate: set[row][21],
+      HTM: set[row][22],
+      VTM: set[row][23],
+    }
+    shots.push(shot)
+  }
+  return shots
+}
