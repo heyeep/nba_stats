a9103202006041d001030aa47bf9d2cf97618723
Hiep Nguyen <hiepism@gmail.com>
2021-10-28 19:16:59 -0700

    Remove boilerplate code

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+import { Container, Row, Col } from 'reactstrap'
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Container>
+        Empty
+      </Container>
+    )
+  }
+}
+
+export default Home
