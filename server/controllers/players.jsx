a81c19c0b06c38f9d6030ff496db4c72507c6f6e
Hiep Nguyen <hiepism@gmail.com>
2021-10-29 20:27:44 -0700

    Change it so that the response is more readable

--- server/controllers/players.jsx
+++ server/controllers/players.jsx
@@ -4,13 +4,22 @@ const api = 'https://data.nba.net/10s/prod/v1/2021'
 
 exports.getPlayers = async (req, res) => {
   const players = await axios.get(`${api}/players.json`)
-                           .then(response => res.status(200).json(response.data))
+                             .then(response => {
+                               const data = { players: response.data.league.standard }
+                               res.status(200).json(data)}
+                             )
                            .catch(err => res.status(442).send(err))
 }
 
-exports.getPlayerByID = async (req, res) => {
+exports.getPlayerById = async (req, res) => {
   const playerId = req.params.id
   const players = await axios.get(`${api}/players/${playerId}_profile.json`)
-                           .then(response => res.status(200).json(response.data))
+                             .then(response => {
+                               const data = {
+                                 personId: playerId,
+                                 stats: response.data.league.standard.stats
+                               }
+                               res.status(200).json(data)
+                             })
                            .catch(err => res.status(442).send(err))
 }
