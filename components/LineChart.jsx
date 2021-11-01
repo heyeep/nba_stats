e682681bb9dc603319ac8ea9d0abc2bffac4ec01
Hiep Nguyen <hiepism@gmail.com>
2021-11-01 08:54:58 -0700

    Add data points and labels to the line graph

--- components/LineChart.jsx
+++ components/LineChart.jsx
@@ -5,6 +5,7 @@ import { LinePath } from '@visx/shape';
 import { scaleLinear } from '@visx/scale';
 import { AxisLeft, AxisBottom } from '@visx/axis';
 import { GridRows, GridColumns } from '@visx/grid';
+import { Text } from '@visx/text'
 import {  MarkerCircle } from '@visx/marker';
 
 export const background = '#f7f7f7';
@@ -22,21 +23,58 @@ export default function LineChart({ width, height, margin = defaultMargin, stats
 
   const yearScale = scaleLinear({
     domain: [
-      Math.min(...stats.map(stat => stat.year)),
-      Math.max(...stats.map(stat => stat.year))
+      Math.min(...stats['ppg'].map(stat => stat.year)),
+      Math.max(...stats['ppg'].map(stat => stat.year))
     ],
   });
 
   const yScale = scaleLinear({
     domain: [
-      Math.min(...stats.map(stat => stat.amount)),
-      Math.max(...stats.map(stat => stat.amount)),
+      0, 50
+      //      Math.min(...stats.map(stat => stat.amount)),
+      //      Math.max(...stats.map(stat => stat.amount)),
     ],
     nice: true,
   });
 
   yearScale.range([0, xMax]);
   yScale.range([yMax, 0]);
+
+//  const scaleMax = Object.keys(stats).map(stat => ())
+  const points = Object.keys(stats).map(stat => (
+    stats[stat].map((d, index) => (
+      <>
+        <Text
+          fontSize={6}
+          dx={yearScale(getX(d))}
+          dy={yScale(getY(d))}
+          x={-5}
+          y={-5}
+        >
+          {d.amount}
+        </Text>
+      <circle
+        key={d + index}
+        r={2}
+        cx={yearScale(getX(d))}
+        cy={yScale(getY(d))}
+        stroke="rgba(33,33,33,0.5)"
+        />
+      </>
+    )))
+  )
+
+  const lines = Object.keys(stats).map((stat, d, index) => (
+    <LinePath
+      key={d + index}
+      data={stats[stat]}
+      curve={curveNatural}
+      x={(d) => yearScale(getX(d)) ?? 0}
+      y={(d) => yScale(getY(d)) ?? 0}
+      stroke="#222"
+      strokeWidth={1.0}
+    />
+  ))
 
   return (
     <div>
@@ -48,27 +86,10 @@ export default function LineChart({ width, height, margin = defaultMargin, stats
           <GridColumns scale={yearScale} width={xMax} height={yMax} stroke="#e0e0e0" />
           <AxisBottom top={yMax} scale={yearScale} numTicks={stats.length} label="Year"/>
           <AxisLeft scale={yScale} />
-          {
-            stats.map((d, index) => (
-              <circle
-                key={d + index}
-                r={3}
-                cx={yearScale(getX(d))}
-                cy={yScale(getY(d))}
-                stroke="rgba(33,33,33,0.5)"
-              />
-            ))}
-          <LinePath
-            data={stats}
-            curve={curveNatural}
-            x={(d) => yearScale(getX(d)) ?? 0}
-            y={(d) => yScale(getY(d)) ?? 0}
-            stroke="#222"
-            strokeWidth={1.5}
-          />
-
+          { lines }
+          { points }
         </Group>
       </svg>
     </div>
-  );
+  )
 }
